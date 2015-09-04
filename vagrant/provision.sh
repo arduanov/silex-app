cd3e6f817a3568cafbf5d80f381e99a6de68cb61
Marsel <arduanov@gmail.com>
2015-09-05 01:10:29 +0500

    bowerphp

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # DEBIAN_FRONTEND=noninteractive
 
+sudo timedatectl set-timezone Europe/Moscow
+
 ##
 # Postgresql
 ##
@@ -23,12 +25,9 @@ service postgresql restart;
 
 echo Installing PHP...
 
-
-apt-get install -yqq php5-cli php5-fpm php5-curl php5-pgsql php5-mcrypt php5-intl libav-tools libxrender1 libfontconfig1
-
+apt-get install -yqq php5-cli php5-fpm php5-curl php5-pgsql php5-mcrypt php5-intl libxrender1 libfontconfig1
 cp /vagrant/vagrant/config/php.ini /etc/php5/fpm/conf.d/
 cp /vagrant/vagrant/config/php.ini /etc/php5/cli/conf.d/
-
 
 
 
@@ -39,8 +38,22 @@ cp /vagrant/vagrant/config/nginx/* /etc/nginx/sites-enabled/
 
 service nginx restart
 
-##
-# composer
-##
+
+echo Installing Composer...
+
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
+
+
+echo Installing BowerPHP...
+
+composer global require "beelab/bowerphp 0.3.*@beta"
+echo "export PATH=~/.composer/vendor/bin:$PATH" > ~/.bash_profile
+source ~/.bash_profile
+
+
+echo Installing Project...
+
+cd /var/www
+composer install
+bowerphp install
